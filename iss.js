a0e5942c9b73a36e0949faecd1c6c52a277a9fc3
ElenaCherpakova <elena.cherpakova@gmail.com>
2021-12-03 04:22:05 +0000

    iss.js

--- iss.js
+++ iss.js
@@ -1,1 +1,26 @@
+const request = require("request");
 
+
+ nextISSTimesForMyLocation((callback) => {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+        return callback(null, nextPasses);
+      });
+    });
+  });
+});
+
+
+module.exports = { nextISSTimesForMyLocation };
