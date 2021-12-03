6d35078bfb1f9746487d9295942ac73672305c2d
ElenaCherpakova <elena.cherpakova@gmail.com>
2021-12-03 15:00:01 +0000

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const { nextISSTimesForMyLocation } = require("./iss");
 
-
-
-const printPassTime = function(passTimes) {
+const printPassTimes = function(passTimes) {
   for (const pass of passTimes) {
     const datetime = new Date(0);
     datetime.setUTCSeconds(pass.risetime);
@@ -15,7 +13,7 @@ nextISSTimesForMyLocation((error, passTimes)=>{
   if (error) {
     return console.log("It didn't work", error);
   }
-  printPassTime(passTimes);
+  printPassTimes(passTimes);
 });
 
 
